7bd00c8cc21f5b4474e5c8b626fff25f28312e2e
Emily Jirles <emily.jirles@gmail.com>
2018-04-20 18:19:50 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,5 +1,16 @@
 // define functions here
 
+function getIt(){
+  return $('p').on('click', function(){ 
+    alert('Hey!');
+    }
+  );
+}
+
+function frameIt(){
+  return $('')
+}
+
 $(document).ready(function(){
 
 // call functions here
